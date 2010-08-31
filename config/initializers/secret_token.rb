48f959ed3e9f153a22496d7722f596c86eeb0976
David <david_zhang_21@yahoo.com>
2010-08-31 14:21:14 -0400

    initial commit for your Rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jdmedia::Application.config.secret_token = 'a5ee3cd0c2e3a407b624b3842ea010efac22efdd0846cfcbd8662ba178635066c8890988aa7ded47761028903e67f99409c4bdf21e03b76ce23c842b4a5252fa'
